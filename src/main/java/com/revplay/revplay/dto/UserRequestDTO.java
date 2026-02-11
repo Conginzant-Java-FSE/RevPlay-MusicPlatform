f66b3aa573269710c87ec55e3ddd6900cae91b80
jayavardhan-jaya <jayavardhanr964@gmail.com>
2026-02-11 21:23:04 +0530

    Added dto and jwt

---
+++ src/main/java/com/revplay/revplay/dto/UserRequestDTO.java
@@ -0,0 +1,26 @@
+package com.revplay.revplay.dto;
+
+import com.revplay.revplay.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotNull
+    private Role role;
+}
---
+++ src/main/java/com/revplay/revplay/security/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.revplay.revplay.security;
+
+import com.revplay.revplay.entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET =
+            "revplay_secret_key_revplay_secret_key_123456";
+
+    private static final long EXPIRATION =
+            1000 * 60 * 60 * 24;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + EXPIRATION)
+                )
+                .signWith(getSigningKey(),
+                        SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
