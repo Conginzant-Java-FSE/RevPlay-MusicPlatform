1bade104eb9cb340558e759300a7b8a59d891050
yasaswini <yasaswinitarigonda@gmail.com>
2026-02-22 21:31:18 +0530

    added album service and controller and also required configuration for  song entity

--- src/main/java/com/revplay/revplay/service/impl/ArtistSocialLinkServiceImpl.java
+++ src/main/java/com/revplay/revplay/service/impl/ArtistSocialLinkServiceImpl.java
@@ -1,4 +1,4 @@
-package com.revplay.revplay.service.Impl;
+package com.revplay.revplay.service.impl;
 import com.revplay.revplay.dto.request.ArtistSocialLinkCreate;
 import com.revplay.revplay.dto.response.ArtistSocialLinkResponse;
 import com.revplay.revplay.entity.Artist;
