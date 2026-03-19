156a72ee961688e49d995441dba1670fb1659299
Harsha Sri Balusu <94100742+Harshasri-Balusu@users.noreply.github.com>
2026-03-19 16:36:06 +0530

    Delete

--- tmp_frontend_src/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://3.7.137.29:8081/api/v1'
-};
--- tmp_frontend_src/src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'bootstrap/dist/js/bootstrap.esm.min.js';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
